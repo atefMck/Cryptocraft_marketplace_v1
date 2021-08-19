1dd9bbdec0ed990b2efd79c729e9b6d544781bbc
atefMck <mechken.atef@gmail.com>
2021-08-19 16:16:38 +0100

    Removing webpack and typescript

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Landing from "./routes/Landing/Landing";
+import "./App.css";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Landing></Landing>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/routes/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderNav from "../../components/Navigation/HeaderNav";
+
+const Landing = () => {
+  return (
+    <div>
+      <HeaderNav />
+    </div>
+  );
+}
+
+export default Landing;
