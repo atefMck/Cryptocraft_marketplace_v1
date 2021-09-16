75d8eb404479b096dced72f174f9496b6d9c2cad
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-09-16 18:49:11 +0000

    Merged Iheb branches

--- src/components/Landings/Header/Header.jsx
+++ src/components/Landings/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import vid from '../../../assets/videos/intro.mp4'
+import headerVideo from '../../../assets/videos/intro.mp4'
 import { StyleSheet, css } from 'aphrodite'
+
 const Header = () =>{
   return(
-    <div className={css(styles.Container)}>
-      <div className={css(styles.divopacity)}></div>
-      <video src={vid} autoPlay loop muted className={css(styles.vid)}></video>
+    <div className={css(styles.container)}>
+      <div className={css(styles.videoOverlay)}></div>
+      <video src={headerVideo} autoPlay loop muted className={css(styles.video)}></video>
       
       <h1>IT'S TIME TO MINE</h1>
       <p>what are you waiting for </p>
@@ -13,30 +14,31 @@ const Header = () =>{
     
     )
   }
-  const styles = StyleSheet.create({
-    Container: {
-      height:'100vh',
-      width:'100vw',
-      display:'flex',
-      flexDirection:'column',
-      justifyContent:'center',
-      alignItems:'center',
-    },
-    vid:{
-      objectFit:'cover',
-      width:'100vw',
-      height:'100vh',
-      position:'fixed',
-      zIndex:'-2'
-    },
-    divopacity:{
-      backgroundColor:'black',
-      position:'fixed',
-      width:'100%',
-      height:'100vh',
-      opacity:'0.4',
-      zIndex:'-1'
-    }
-    
-  }) 
-  export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    height:'100vh',
+    width:'100vw',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  video:{
+    objectFit:'cover',
+    width:'100vw',
+    height:'100vh',
+    position:'fixed',
+    zIndex:'-2'
+  },
+  videoOverlay:{
+    backgroundColor:'black',
+    position:'fixed',
+    width:'100%',
+    height:'100vh',
+    opacity:'0.4',
+    zIndex:'-1'
+  }
+})
+
+export default Header;
